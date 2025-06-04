4313b65eb9c7503aaf6554873f00d74b9f6ed40d
risman_doank <permonicity@gmail.com>
2025-06-04 11:06:34 +0700

    comit hiden action inject admin

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -22,7 +22,8 @@ class SiteController extends Controller
         return [
             'access' => [
                 'class' => AccessControl::class,
-                'except' => ['login','createadmin'],
+                // 'except' => ['login','createadmin'],
+                'except' => ['login'],
                 'rules' => [
                     [
                         'allow' => true,
@@ -141,36 +142,36 @@ class SiteController extends Controller
         return $data;
     }
 
-    public function actionCreateadmin(){
-        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
-        $response = [
-            'status'=>false,
-            'message'=>'Admin Exist'
-        ];
-        $model = User::find()->where(['username' => 'admin'])->one();
+    // public function actionCreateadmin(){
+    //     \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+    //     $response = [
+    //         'status'=>false,
+    //         'message'=>'Admin Exist'
+    //     ];
+    //     $model = User::find()->where(['username' => 'admin'])->one();
 
-        if(empty($model)){
-            $model = new User();
-            $model->username = 'admin';
-            $model->email = 'admin@admin.com';
-            $model->full_name = 'Administrator';
-            $model->tipe = 1;
-            $model->password = 'admin123';
-            $model->created_at = time();
-            $model->updated_at = time();
+    //     if(empty($model)){
+    //         $model = new User();
+    //         $model->username = 'admin';
+    //         $model->email = 'admin@admin.com';
+    //         $model->full_name = 'Administrator';
+    //         $model->tipe = 1;
+    //         $model->password = 'admin123';
+    //         $model->created_at = time();
+    //         $model->updated_at = time();
 
-            if($model->save()){
-                $response = [
-                    'status'=>true,
-                    'message'=>'Create Admin Success'
-                ];
-            }else{
-                var_dump($model->getErrors());die();
-            }
+    //         if($model->save()){
+    //             $response = [
+    //                 'status'=>true,
+    //                 'message'=>'Create Admin Success'
+    //             ];
+    //         }else{
+    //             var_dump($model->getErrors());die();
+    //         }
 
             
-        }
+    //     }
 
-        return $response;
-    }
+    //     return $response;
+    // }
 }
