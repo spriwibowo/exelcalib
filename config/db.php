b9dcda2a2df3624d2993a4e8099156bfa46512f2
risman_doank <permonicity@gmail.com>
2025-06-13 10:07:53 +0700

    comit db overwrite

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 
-return [
+$dbconf = [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=exelcalib',
     'username' => 'root',
@@ -12,3 +12,8 @@ return [
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
 ];
+
+if(file_exists(__DIR__ . '/db.overwrite.php')) 
+	include(__DIR__ . '/db.overwrite.php');
+	
+return $dbconf;
